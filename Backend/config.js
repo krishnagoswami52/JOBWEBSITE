d9eef6f2de2257297cd39ac0a3ba59ae4438077f
unknown <krishnagoswami.52@gmail.com>
2020-06-30 01:50:24 +0530

    Second Commit

---
+++ Backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    secret: 'ING@345',
+    MONGO_URI: 'mongodb+srv://Krishna09:mnikg@cluster0-0pkqr.mongodb.net/jobsportal?retryWrites=true&w=majority' 
+
+
+
+    
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  ApiUrl:"http://localhost:3000/"
+};
+
